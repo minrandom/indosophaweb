2caaef16dc6d5b3eac1e43b21b7424dea87fe8e6
Jojo Pattinama <pattinamajojo@gmail.com>
2023-04-18 14:22:55 +0700

    mantap

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include 'kepala.php';
+?>
+
+<body>
+
+<?php
+include 'menubar.php';
+
+//include 'content1.php';
+include 'about.php';
+include 'footer.php';
+
+?>
+
+</body>
+</html>
